28a3401e417ba27aec6db96e8e3c26f6a2ee5eab
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-02-13 21:02:59 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/MyButton.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class MyButton 
+    {
+        protected Point TopLeft;
+        protected Point BottomRight;
+        private int Width;
+        private int Height;
+
+        internal MyButton(Point topLeft, Point bottomRight) 
+        {
+            this.TopLeft = topLeft;
+            this.BottomRight = bottomRight;
+            SetBottomRight(bottomRight);
+            SetTopLeft(topLeft);
+        }
+        internal int GetWidth()
+        {
+            return Width;
+        }
+        internal int GetHeight()
+        {
+            return Height;
+        }
+        internal bool SetTopLeft(Point TopLeft)
+        {
+            if (TopLeft.GetX() < this.BottomRight.GetX() && TopLeft.GetY() > this.BottomRight.GetY())
+            {
+                this.Height = TopLeft.GetY() - BottomRight.GetY();
+                this.Width = BottomRight.GetX() - TopLeft.GetX();
+                return true;
+            }
+            return false;
+        }
+        internal bool SetBottomRight(Point BottomRight)
+        {
+            if (BottomRight.GetX() > this.TopLeft.GetX() && BottomRight.GetY() < this.TopLeft.GetY())
+            {
+                this.Height = TopLeft.GetY() - BottomRight.GetY();
+                this.Width = BottomRight.GetY() - TopLeft.GetX();
+                return true;
+            }
+            return false;
+        }
+        internal Point GetTopLeft()
+        {
+            return TopLeft;
+        }
+        internal Point GetBottomRight()
+        {
+            return BottomRight;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/MyCanvas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class MyCanvas
+    {
+        public const int MaxWidth = 800;
+        public const int MaxHeight = 600;
+        private static int ButtonIndex = 0;
+        private static MyButton[] buttons = new MyButton[MaxButtons];
+        private static int MaxButtons = 3;
+
+        public static bool CreateNewButton(int x1, int y1, int x2, int y2)
+        {
+            if (ButtonIndex == MaxButtons)
+            {
+                return false;
+            }
+
+
+            if (x1 <= MaxWidth && y1 <= MaxHeight && x2 <= MaxWidth && y2 <= MaxHeight)
+            {
+                Point p1 = new Point(x1, y2);
+                Point p2 = new Point(x2, y2);
+                MyButton Btn1 = new MyButton(p1, p2);
+
+                ButtonIndex++;
+                return true;
+            }
+            return false;
+        }
+
+
+        }
+
+    }
+}
