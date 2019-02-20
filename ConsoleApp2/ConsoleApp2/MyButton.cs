1f14ec93d56b94f502619de68d54f80d66edb65e
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-02-20 16:35:43 +0200

    Update MyButton.cs

--- ConsoleApp2/ConsoleApp2/MyButton.cs
+++ ConsoleApp2/ConsoleApp2/MyButton.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    class MyButton 
+    internal class MyButton 
     {
         protected Point TopLeft;
         protected Point BottomRight;
